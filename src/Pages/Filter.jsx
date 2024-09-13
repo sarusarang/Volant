b99520673fde5303177dc892570dec352f9f5ee5
sarusarang <sarangsaru445@gmail.com>
2024-09-13 09:59:57 +0530

    updated

--- src/Pages/Filter.jsx
+++ src/Pages/Filter.jsx
@@ -173,7 +173,7 @@ function Filter() {
             try {
 
 
-                if (Data.Size || Data.color) {
+                if (Data.category) {
 
 
                     const res = await GetFilter(Data.category, Data.Size, Data.color)
@@ -244,7 +244,7 @@ function Filter() {
     }
 
 
-    const[open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
     const [expandedCategory, setExpandedCategory] = useState(false);
     const [expandedType, setExpandedType] = useState(false);
 
@@ -294,7 +294,7 @@ function Filter() {
                                     <Accordion.Item eventKey="0">
 
                                         <Accordion.Header onClick={() => setExpandedCategory(!expandedCategory)}>
-                                            Categories 
+                                            Categories
                                         </Accordion.Header>
 
                                         <Accordion.Body>
@@ -351,7 +351,7 @@ function Filter() {
                                     <Accordion.Item eventKey="1">
 
                                         <Accordion.Header onClick={() => setExpandedType(!expandedType)}>
-                                            Type 
+                                            Type
                                         </Accordion.Header>
 
                                         <Accordion.Body>
@@ -368,14 +368,14 @@ function Filter() {
                                                 <label className="form-check-label" htmlFor="sandals">Sandals</label>
                                             </div>
 
-                                            
+
                                             <div className="form-check">
                                                 <input
-                                                    checked={Data.type === "flip flop"}
-                                                    onChange={() => SetData({ ...Data, type: "flip flop" })}
+                                                    checked={Data.type === "flipflops"}
+                                                    onChange={() => SetData({ ...Data, type: "flipflops" })}
                                                     className="form-check-input"
                                                     type="checkbox"
-                                                    value="flip flop"
+                                                    value="flipflops"
                                                     id="flipFlop"
                                                 />
                                                 <label className="form-check-label" htmlFor="flipFlop">Flip Flop</label>
@@ -384,11 +384,11 @@ function Filter() {
 
                                             <div className="form-check">
                                                 <input
-                                                    checked={Data.type === "slipper"}
-                                                    onChange={() => SetData({ ...Data, type: "slipper" })}
+                                                    checked={Data.type === "slippers"}
+                                                    onChange={() => SetData({ ...Data, type: "slippers" })}
                                                     className="form-check-input"
                                                     type="checkbox"
-                                                    value="slipper"
+                                                    value="slippers"
                                                     id="slipper"
                                                 />
                                                 <label className="form-check-label" htmlFor="slipper">Slipper</label>
@@ -397,41 +397,66 @@ function Filter() {
 
                                             <div className="form-check">
                                                 <input
-                                                    checked={Data.type === "clogs"}
-                                                    onChange={() => SetData({ ...Data, type: "clogs" })}
+                                                    checked={Data.type === "flatshoes"}
+                                                    onChange={() => SetData({ ...Data, type: "flatshoes" })}
                                                     className="form-check-input"
                                                     type="checkbox"
-                                                    value="clogs"
+                                                    value="flatshoes"
                                                     id="clogs"
                                                 />
-                                                <label className="form-check-label" htmlFor="clogs">Clogs</label>
+                                                <label className="form-check-label" htmlFor="clogs">Flat Shoes</label>
                                             </div>
 
 
                                             <div className="form-check">
                                                 <input
-                                                    checked={Data.type === "casual"}
-                                                    onChange={() => SetData({ ...Data, type: "casual" })}
+                                                    checked={Data.type === "casualshoes"}
+                                                    onChange={() => SetData({ ...Data, type: "casualshoes" })}
                                                     className="form-check-input"
                                                     type="checkbox"
-                                                    value="casual"
+                                                    value="casualshoes"
                                                     id="casual"
                                                 />
-                                                <label className="form-check-label" htmlFor="casual">Casual</label>
+                                                <label className="form-check-label" htmlFor="casual">Casual Shoes</label>
                                             </div>
 
 
                                             <div className="form-check">
                                                 <input
-                                                    checked={Data.type === "sports"}
-                                                    onChange={() => SetData({ ...Data, type: "sports" })}
+                                                    checked={Data.type === "specialcollections"}
+                                                    onChange={() => SetData({ ...Data, type: "specialcollections" })}
                                                     className="form-check-input"
                                                     type="checkbox"
-                                                    value="sports"
+                                                    value="specialcollections"
                                                     id="sports"
                                                 />
-                                                <label className="form-check-label" htmlFor="sports">Sports</label>
+                                                <label className="form-check-label" htmlFor="sports">Special Collections</label>
                                             </div>
+
+
+
+                                            
+
+
+                                            <div className="form-check">
+
+                                                <input
+
+                                                    checked={Data.type === "schooledition"}
+                                                    onChange={() => SetData({ ...Data, type: "schooledition" })}
+                                                    className="form-check-input"
+                                                    type="checkbox"
+                                                    value="schooledition"
+                                                    id="schooledition"
+
+                                                />
+
+                                                <label className="form-check-label" htmlFor="sports">School Edition</label>
+
+                                            </div>
+
+
+
 
 
                                         </Accordion.Body>
@@ -630,7 +655,7 @@ function Filter() {
 
                                             <div>
 
-                                                <img src="https://www.lookshopbd.com/website/images/no_result.gif" className='img-fluid' alt="" />
+                                                <img src="https://pinkwardrobe.in/images/NoProduct.jpg" className='img-fluid' alt="" />
 
                                             </div>
 
@@ -654,296 +679,7 @@ function Filter() {
                 </div>
 
 
-
-
-
             </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            {/* <section className='w-100 container p-5 mt-3 mb-5'>
-
-
-                <div className='row'>
-
-
-                    <div className='col-md-3 d-flex flex-column'>
-
-
-                        <button className='fw-bold mb-4 btn border' style={{ cursor: 'pointer' }} onClick={() => { SetStatus(!Status) }}>Categories{Status ? <i class="fa-solid fa-angle-up ms-2"></i> : <i class="fa-solid fa-angle-down ms-2"></i>} </button>
-
-
-                        {
-
-                            Status &&
-
-
-                            <div className=' d-flex flex-column'>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("gents", "sandals") }}>Gents Sandels</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("gents", "slipres") }}>Gents Slipper and Flip Flops</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("gents", "casualshoes") }}>Gents Casual Shoes</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("gents", "specialcollections") }}>Gents Special Collections</a>
-
-
-                                <a className='fil-link' onClick={() => { handleFilterData("ladies", "sandals") }}>Ladies Sandels</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("ladies", "slipres") }}>Ladies Slipper and Flip Flops</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("ladies", "casualshoes") }}>Ladies Casual Shoes</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("ladies", "flatshoes") }}>Ladies Flat Shoes</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("ladies", "specialcollections") }}>Ladies Special Collections</a>
-
-
-                                <a className='fil-link' onClick={() => { handleFilterData("boys&girls", "sandals") }}>Boys & Girls Sandels</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("boys&girls", "shoes") }}>Boys & Girls Shoes</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("boys&girls", "schooledition") }}>Boys & Girls School Edition</a>
-
-
-                                <a className='fil-link' onClick={() => { handleFilterData("kids", "sandals") }}>kids Sandels</a>
-
-                                <a className='fil-link' onClick={() => { handleFilterData("kids", "shoes") }}>kids Shoes</a>
-
-                            </div>
-                        }
-
-                    </div>
-
-
-
-                    <div className='col-md-9'>
-
-
-                        <div className='row'>
-
-
-                            <div className='col-md-6 mb-3'>
-
-
-                                <select name="" id="" className='form-control custom-select' onChange={(e) => { SetData({ ...Data, color: e.target.value }) }}>
-
-
-                                    <option value="">Select Your Color</option>
-                                    <option value="Black">Black</option>
-                                    <option value="Blue">Blue</option>
-                                    <option value="Brown">Brown</option>
-                                    <option value="Camel">Camel</option>
-                                    <option value="Cherry">Cherry</option>
-                                    <option value="Gold">Gold</option>
-                                    <option value="Grape">Grape</option>
-                                    <option value="Green">Green</option>
-                                    <option value="Grey">Grey</option>
-                                    <option value="Maroon">Maroon</option>
-                                    <option value="Mehandi">Mehandi</option>
-                                    <option value="Navy">Navy</option>
-                                    <option value="Olive">Olive</option>
-                                    <option value="Peach">Peach</option>
-                                    <option value="Peacock">Peacock</option>
-                                    <option value="Pink">Pink</option>
-                                    <option value="Purple">Purple</option>
-                                    <option value="Tan">Tan</option>
-                                    <option value="Violet">Violet</option>
-                                    <option value="White">White</option>
-                                    <option value="Wine">Wine</option>
-                                    <option value="Yellow">Yellow</option>
-                                    <option value="sky blue">sky blue</option>
-                                    <option value="fblack">fblack</option>
-                                    <option value="Lemon">Lemon</option>
-
-
-                                </select>
-
-                            </div>
-
-
-
-
-                            <div className='col-md-6 mb-3'>
-
-
-                                <select name="" id="" className='form-control custom-select' onChange={(e) => { SetData({ ...Data, Size: e.target.value }) }}>
-
-
-                                    <option value="">Select the Size</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                    <option value="13">13</option>
-
-
-                                </select>
-
-                            </div>
-
-
-                        </div>
-
-
-
-                        <div className='row p-3'>
-
-
-                            {
-
-                                Loading ?
-
-                                    Array.from({ length: 6 }).map((item) => (
-
-
-                                        <div className=' mt-3 col-md-4 '>
-
-                                            <Skeleton sx={{ height: 190 }} width={'100%'} animation="wave" variant="rectangular" />
-
-                                            <Skeleton animation="wave" height={20} width={'100%'} style={{ marginBottom: 6, marginTop: '1rem' }} />
-
-                                            <Skeleton animation="wave" height={20} width="80%" />
-
-                                        </div>
-
-                                    ))
-
-                                    :
-
-
-                                    FilterData.length > 0 ?
-
-                                        FilterData.map((item) => (
-
-
-                                            <div className="col-lg-3 col-md-6 col-sm-6 d-flex">
-
-                                                <div className="card w-100 my-2 hover-shdw">
-
-
-                                                    <div className='new-arrival' onClick={() => { Navigate(`/pro/${item.id}`) }}>
-
-                                                        <img loading='lazy' src={item.image} className="card-img-top" style={{ cursor: 'pointer' }} />
-
-                                                        {/* {item.new_arrival ? <span className="badge">New Arrival</span> : ""} */}
-
-
-            {/* </div>
-
-
-
-                                                    <div className="card-body d-flex flex-column">
-
-                                                        <p className="mb-1 me-1">{item.category} {item.sub_cateory} <span className='fw-bold'>{item.name}</span></p>
-
-                                                        <div className="d-flex flex-row ">
-
-                                                            <p className="mb-1 me-2">₹{item.offer_is_available ? item.offer_price : item.price}</p>
-
-                                                            <span className="text-danger" style={{ textDecoration: 'line-through' }}><s>{item.offer_is_available ? <s> ₹{item.price} </s> : ""}</s></span>
-
-                                                            <p className='text-success ms-2'>{item.offer_is_available ? <s style={{ textDecoration: 'none' }}>{item.offer_percentage}%off</s> : ""}</p>
-
-
-                                                        </div>
-
-
-
-
-
-                                                    </div>
-
-
-                                                </div>
-
-                                            </div>
-
-
-                                        ))
-
-                                        :
-
-                                        <div className='d-flex justify-content-center'>
-
-                                            <img src="https://www.fruitomans.com/themes/home/images/no-product-found.png" loading='lazy' alt="img" className='img-fluid' />
-
-                                        </div>
-
-
-                            }
-
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-
- */}
-
-            {/* </section> */}
-
-
-
-
 
 
         </>
